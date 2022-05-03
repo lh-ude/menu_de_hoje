4c7322b2c022088ccd459054e00b35609acb7225
Luíza Araújo <99282366+lh-ude@users.noreply.github.com>
2022-05-03 17:21:53 -0300

    update 03-05-2022 - aprimoraento do script de sorteio

--- assets/js/menu_dia.js
+++ assets/js/menu_dia.js
@@ -2,8 +2,8 @@ function sorteio(){
 var comida = ['Fast food','Massa','Asiática','Brasileira','Natural','Frutos do mar'];
 var tipo_comida = ['gourmet','saudável','exótica','caseira','novidade','temperada'];
 
-var resultado1 = Math.floor(Math.random(comida) * 6);
-var resultado2 = Math.floor(Math.random(tipo_comida) * 6);
+var resultado1 = Math.floor(Math.random(comida) * comida.length);
+var resultado2 = Math.floor(Math.random(tipo_comida) * comida.length);
 
 document.getElementById("resultadoFinal").innerHTML = (comida[resultado1]) + " e " + (tipo_comida[resultado2]);
 }
