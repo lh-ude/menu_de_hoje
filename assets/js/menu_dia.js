efce1bd6acd3a0e64735f9f1040e995eb7c16873
Luíza Araújo <99282366+lh-ude@users.noreply.github.com>
2022-05-25 22:58:47 -0300

    update 25-05 - novas categorias e mudança no JS

--- assets/js/menu_dia.js
+++ assets/js/menu_dia.js
@@ -1,9 +1,9 @@
 function sorteio(){
-var comida = ['Fast food','Massa','Asiática','Brasileira','Natural','Frutos do mar'];
-var tipo_comida = ['gourmet','saudável','exótica','caseira','novidade','temperada'];
+const comida = ['Fast food','Massa','Asiática','Brasileira', 'Carne', 'Frutos do mar'];
+const tipo_comida = ['gourmet','saudável','exótica','caseira','de um novo restaurante','temperada','com sobremesa'];
 
-var resultado1 = Math.floor(Math.random(comida) * comida.length);
-var resultado2 = Math.floor(Math.random(tipo_comida) * comida.length);
+const resultado1 = Math.floor(Math.random(comida) * comida.length);
+const resultado2 = Math.floor(Math.random(tipo_comida) * tipo_comida.length);
 
-document.getElementById("resultadoFinal").innerHTML = (comida[resultado1]) + " e " + (tipo_comida[resultado2]);
+document.getElementById("resultadoFinal").innerHTML = (comida[resultado1]) + " " + (tipo_comida[resultado2]);
 }
